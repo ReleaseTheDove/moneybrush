1c7d533631dcbfd46cbae8ec94895385cd25eea8
simon <coutingstars1@163.com>
2018-06-05 22:00:31 +0800

    solve mysql wait timeout

--- absgamer/absgamer/__init__.py
+++ absgamer/absgamer/__init__.py
@@ -1,7 +1,12 @@
 from flask import Blueprint
-
+from models import session
 
 api = Blueprint('api', __name__)
 
+
+@api.teardown_request
+def handle_teardown_request(exception):
+    "Dealing at the aftermath."
+    session.remove()
 
 from absgamer.views import article, game
