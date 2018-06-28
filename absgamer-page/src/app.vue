2cd87a1c70c2c9ea731b7249e4e5b30050c7996d
simon <coutingstars1@163.com>
2018-06-29 02:54:20 +0800

    the fucking vue config is almost over

---
+++ absgamer-page/src/app.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="test">{{text}}</div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      text: 'func'
+    }
+  }
+}
+</script>
+
+<style>
+#test{
+  color: green;
+}
+</style>
+
